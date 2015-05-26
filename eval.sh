0282722a04a2e3e9b1480b6b53d3788c1aa1a2b3
Philip Moss Robinson <probinson@localhost.localdomain>
2015-05-26 15:02:09 -0700

    cleaner error handling, shouldn't change behaviour

--- eval.sh
+++ eval.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 
+
 if (( $# != 3 )); then
     printf "Usage: %s solution_output eval_data resultant_path \n" "$0" >&2
     exit 1
@@ -10,9 +11,11 @@ solution=$1
 data=$2
 result=$3
 
+set -x
+
 # test files
-test_file=$data/test.txt
-gold_file=$data/gold.txt
+test_file=$(ls $data/*test.txt | head -n 1)
+gold_file=$(ls $data/*gold.txt | head -n 1)
 
 if ! [[ -e ${test_file} ]]; then
    echo "${test_file} does not exist"
@@ -55,13 +58,24 @@ jarfile=lib/BerkeleyParser-1.7.jar
 # read and dump grammar object
 echo "Reading grammar ..."
 java -cp $jarfile edu.berkeley.nlp.PCFGLA.ReadGrammar -gr $rules -lex $lexicon -st $states -out $grammar.gr
+rc=$?
+echo "reading grammar exited with return code $rc"
+if [ $rc -ne 0 ]; then exit $rc; fi
 
 # parse
 echo "Parsing ..."
 java -jar $jarfile -noHierarchy -gr $grammar.gr -inputFile ${test_file} -nThreads 1 -outputFile ${parsed_file}
+rc=$?
+echo "parsing exited with return code $rc"
+if [ $rc -ne 0 ]; then exit $rc; fi
+
 
 # evaluation
 echo "Evaluating ..."
 $evalb -p ${evalb_param} ${gold_file} ${parsed_file} > ${eval_file} 
+rc=$?
+echo "evaluatiing exited with return code $rc"
+if [ $rc -ne 0 ]; then exit $rc; fi
+
 tail -n 6 ${eval_file} | head -n 1 | awk '{print $4}' > ${f1_file}
 echo 'F1 Measure: ' $(cat ${f1_file}) 
